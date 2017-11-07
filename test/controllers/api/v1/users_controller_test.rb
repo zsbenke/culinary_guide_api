6d7f560e50ff947f5458c0ee350c7ec0d3709d3b
Zsolt Benke <zsolt@decoding.io>
2017-11-07 20:38:31 +0100

    Feliratkozás állapot megjelenítése az API-ban

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -9,19 +9,27 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
       get api_v1_user_details_path, params: nil, headers: authorization_header(token)
     end
 
+    user = User.find_by_unique_hash(unique_hash)
     json = JSON.parse response.body
+
     assert_response :success
     assert_equal unique_hash, json['unique_hash']
+    assert_equal user.subscriber?, json['subscriber']
+    assert_nil json['expires_at']
   end
 
   test "should permit details for existing user" do
     user = users :user
     token = Token.encode({ unique_hash: user.unique_hash })
+    user.update expires_at: 1.week.from_now
+
     get api_v1_user_details_path, params: nil, headers: authorization_header(token)
 
     json = JSON.parse response.body
     assert_response :success
     assert_equal user.unique_hash, json['unique_hash']
+    assert_equal user.subscriber?, json['subscriber']
+    assert_equal user.expires_at.to_s, Time.parse(json['expires_at']).in_time_zone.to_s
   end
 
   test "should deny details for bad token" do
