b4ff4f6df744583f86cf64a5836b6935693a87af
Zsolt Benke <zsolt@decoding.io>
2017-11-07 19:51:46 +0100

    Felhasználó auth token alapján

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   validates :unique_hash, uniqueness: true
+
+  def self.authenticate(token, options = {})
+    payload = Token.decode(token)
+    return if payload.nil? || payload.try(:[], 'unique_hash').nil?
+    find_or_create_by(unique_hash: payload['unique_hash'])
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,4 +8,34 @@ class UserTest < ActiveSupport::TestCase
     assert_not user_2.valid?
     assert_equal :taken, user_2.errors.details[:unique_hash][0][:error]
   end
+
+  test "should existing authenticate user" do
+    user_1 = users :user
+    token = Token.encode({ unique_hash: user_1.unique_hash })
+    current_user = User.authenticate(token)
+
+    assert_equal user_1, current_user
+  end
+
+  test "should create non-existing user" do
+    unique_hash = 'hash124'
+    token = Token.encode({ unique_hash: unique_hash })
+    current_user = User.authenticate(token)
+
+    assert_equal User.find_by_unique_hash(unique_hash), current_user
+  end
+
+  test "should return nothing for bad token" do
+    token = 'b4dt0ken'
+    current_user = User.authenticate(token)
+
+    assert_nil current_user
+  end
+
+  test "should return nothing for missing unique_hash key in encoded token" do
+    token = Token.encode({ foo: 'bar' })
+    current_user = User.authenticate(token)
+
+    assert_nil current_user
+  end
 end
