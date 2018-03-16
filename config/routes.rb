3bd0fff9370a2fda1246118558cecf8e0f3780a2
Zsolt Benke <zsolt@decoding.io>
2018-03-16 20:19:31 +0100

    API elérés módosítása

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1, defaults: { format: :json } do
-      get :user_details, to: 'users#details'
-      resources :restaurants, only: [:index, :show] do
-        get :autocomplete, on: :collection
-      end
+  namespace :v1, defaults: { format: :json } do
+    get :user_details, to: 'users#details'
+    resources :restaurants, only: [:index, :show] do
+      get :autocomplete, on: :collection
     end
   end
 end
